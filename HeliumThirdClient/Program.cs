6176910efe16e922a83473500ea7db2efd805966
Domantas Jadenkus <djadenkus@gmail.com>
2016-02-04 20:26:09 +0200

    Base project structure

---
+++ HeliumThirdClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HeliumThirdClient
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameHelium())
+                game.Run();
+        }
+    }
+#endif
+}
---
+++ HeliumThirdServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HeliumThirdServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
