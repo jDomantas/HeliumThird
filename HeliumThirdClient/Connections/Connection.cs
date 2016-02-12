b9fd68d9a0ef227ed765cdcfb79d7ac339d365de
Domantas Jadenkus <djadenkus@gmail.com>
2016-02-12 17:45:27 +0200

    added comments to client connection class

--- HeliumThirdClient/Connections/Connection.cs
+++ HeliumThirdClient/Connections/Connection.cs
@@ -4,9 +4,27 @@ namespace HeliumThirdClient.Connections
 {
     abstract class Connection
     {
+        /// <summary>
+        /// Updates the internal connection state
+        /// </summary>
+        /// <param name="delta">Seconds passed since last update</param>
         public abstract void Update(double delta);
+
+        /// <summary>
+        /// Get next event from this connection
+        /// </summary>
+        /// <returns></returns>
         public abstract Event ReadMessage();
+
+        /// <summary>
+        /// Send event to the server
+        /// </summary>
+        /// <param name="e"></param>
         public abstract void SendMessage(Event e);
+
+        /// <summary>
+        /// Leave current game
+        /// </summary>
         public abstract void LeaveGame();
     }
 }
